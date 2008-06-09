7898a23c5a55c5490a43f6b61a3116e005fe4319
Michael R. Davis <mrdvt@cpan.org>
2008-06-09 19:11:57 +0000

    Import of MRDVT/Geo-Horizon-0.02 from CPAN.
    gitpan-cpan-distribution: Geo-Horizon
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MRDVT/Geo-Horizon-0.02.tar.gz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

--- lib/Geo/Horizon.pm
+++ lib/Geo/Horizon.pm
@@ -17,13 +17,13 @@ Geo::Horizon - Calculate distance to the visual horizon
 
 =head1 DESCRIPTION
 
-A perl object of calculating the distance to the visual horizon on an ellipsoid.
+A perl object for calculating the distance to the visual horizon on an ellipsoid.
 
 =cut
 
 use strict;
 use vars qw($VERSION);
-$VERSION = sprintf("%d.%02d", q{Revision: 0.01} =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q{Revision: 0.02} =~ /(\d+)\.(\d+)/);
 
 =head1 CONSTRUCTOR
 
@@ -53,7 +53,7 @@ sub initialize {
 
 =head2 ellipsoid
 
-Method to set or retrieve the current ellipsoid object.  The ellipsoid is a Geo::Ellipsoids object.
+Method to set or retrieve the current ellipsoid object.  The ellipsoid is a L<Geo::Ellipsoids> object.
 
   my $ellipsoid=$gh->ellipsoid;  #Default is WGS84
 
@@ -75,10 +75,12 @@ sub ellipsoid {
 
 =head2 distance
 
-The straight-line of sight distance to the horizon:
+The straight-line of sight distance to the horizon: This formula does not take in account radio or optical refraction which will be further the longer the wavelength.
 
-  my $dist=$obj->distance($alt, $lat);
-  my $dist=$obj->distance($alt);       #default lat => 0
+  my $dist=$obj->distance($alt, $lat);  #alt in meters (ellipsoid units)
+                                        #lat in signed decimal degrees
+  my $dist=$obj->distance($alt);        #default lat => 0 (equator)
+  my $dist=$obj->distance;              #default alt => 1.7
 
 Formula from http://newton.ex.ac.uk/research/qsystems/people/sque/physics/horizon/
 
@@ -96,13 +98,13 @@ sub distance {
   return sqrt($alt * (2 * $R + $alt));
 }
 
-=head2
+=head2 distance_great_circle
 
-The curved distance along the ellipsoid to the horizon:
+The curved distance along the ellipsoid to the horizon:  This is the great circle distance from the track point snapped to the ellipsoid to the visual horizon of the observer.
 
   my $dist=$obj->distance_great_circle($alt, $lat);
-  my $dist=$obj->distance_great_circle($alt);
-  my $dist=$obj->distance_great_circle();
+  my $dist=$obj->distance_great_circle($alt);  #default lat => 0
+  my $dist=$obj->distance_great_circle();      #default alt => 1.7
 
 Formula from http://newton.ex.ac.uk/research/qsystems/people/sque/physics/horizon/
 
@@ -145,3 +147,4 @@ it under the same terms as Perl itself.
 =head1 SEE ALSO
 
 Geo::Ellipsoids
+Math::Trig
